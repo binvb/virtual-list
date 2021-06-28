400c6f1064fe59da567042a322a1f43e2185a892
vb <vb@itech8.net>
2021-06-28 15:44:57 +0800

    init

---
+++ packages/vue-virtual-scroll/example/vue-virtual-scroll-demo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <VueVirtualScroll
+    :scrollItem=ScrollItem
+    :tombstoneItem=Tombstone
+     />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import VueVirtualScroll from './../../../index.vue'
+import ScrollItem from './component/scrollItem.vue'
+import Tombstone from './component/tombstone.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    VueVirtualScroll
+  },
+  setup: () => {
+    return {
+      ScrollItem,
+      Tombstone
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ packages/vue-virtual-scroll/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div></div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: 'vue-virtual-scroll',
+    props: {
+        scrollItem: { // scroll list item   
+            type: HTMLElement,
+            required: true
+        },
+        tombstoneItem: { // tombStone item which equal lazyload
+            type: HTMLElement,
+            required: true
+        },
+        RUNWAY_ITEMS: { // Number of items to instantiate beyond current view in the scroll direction.
+            type: Number,
+            required: false
+        },
+        RUNWAY_ITEMS_OPPOSITE: {// Number of items to instantiate beyond current view in the opposite direction.
+            type: Number,
+            required: false
+        }
+    },
+    setup: () => {
+        return {
+            
+        }
+    }
+})
+</script>
