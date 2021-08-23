0833c38382042982f515551a600401920cf2deb3
vb <vb@itech8.net>
2021-08-23 11:40:39 +0800

    vb

---
+++ packages/react-virtual-scroll/example/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import VirtualList from './../../index'
+
+function App() {
+  return (
+    <div className="App">
+      <VirtualList></VirtualList>
+    </div>
+  );
+}
+
+export default App;
